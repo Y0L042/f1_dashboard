786758b93058758810f3a773169a49cec8aa76ce
Y0L042 <66077762+Y0L042@users.noreply.github.com>
2023-06-04 11:13:32 +0200

    Added check for login
    if user is logged in, go to home, otherwise show login

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,8 @@ import 'dart:math' as math;
 // Import pages
 import 'package:f1_dashboard/pages/constructorPage.dart';
 import 'package:f1_dashboard/pages/login_page.dart';
+import 'package:f1_dashboard/pages/dashboard.dart';
+import 'package:f1_dashboard/pages/user_profile_page.dart';
 
 String renderServerURL = 'https://f1stats-flask-server.onrender.com';
 
@@ -15,7 +17,30 @@ void main() {
   runApp(F1StatsApp());
 }
 
-class F1StatsApp extends StatelessWidget {
+class F1StatsApp extends StatefulWidget {
+  @override
+  _F1StatsAppState createState() => _F1StatsAppState();
+}
+
+class _F1StatsAppState extends State<F1StatsApp> {
+  bool isLoggedIn = false; // Assume the user is not logged in initially
+
+  @override
+  void initState() {
+    super.initState();
+    checkLoggedInStatus();
+  }
+
+  Future<void> checkLoggedInStatus() async {
+    // TODO: Implement your login status check logic here
+    // For example, you can check if the user has a valid token or session
+    // and update the 'isLoggedIn' variable accordingly.
+    // You can use SharedPreferences, secure storage, or any other method to store and retrieve the login status.
+    // Example:
+    // isLoggedIn = await checkIfUserIsLoggedIn();
+    isLoggedIn = true; // Assume the user is logged in for demonstration purposes
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,7 +48,13 @@ class F1StatsApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      // Define the routes and link them to the respective Dart files
+      initialRoute: isLoggedIn ? '/dashboard' : '/',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/dashboard': (context) => Dashboard(),
+        '/profile': (context) => UserProfilePage(),
+      },
     );
   }
 }
